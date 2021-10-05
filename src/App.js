f58cfd6ee5ff7e22adb2f18602705c2093797487
Andrew Sharkey <sharkand@hotmail.com>
2021-10-05 22:37:44 +0100

    Added header with logo.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Header from './components/Header';
 import NavBar from './components/NavBar';
-import SwimMeet from './components/swim-meet/SwimMeet';
 import Home from './components/home/Home';
+import SwimMeet from './components/swim-meet/SwimMeet';
 
 function App() {
   return (
     <>
+      <Header />
       <NavBar />
       <Router>
         <Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../assets/logo.jpg';
+
+const Header = () => {
+
+    return (
+        <header>
+            <img src={Logo} alt='Logo'/>
+        </header>
+    )
+}
+
+export default Header;
