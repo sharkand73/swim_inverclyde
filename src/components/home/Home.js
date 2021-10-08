f8e0b47ea8d803ad3fd0a14418469e7f29345dd9
Andrew Sharkey <sharkand@hotmail.com>
2021-10-08 20:48:36 +0100

    Solved styling problem with react-awesome-carousel, added wallpaper background.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,57 +1,25 @@
 import React from 'react';
-import Carousel from 'react-multi-carousel' 
-import 'react-multi-carousel/lib/styles.css';
+import AwesomeSlider from 'react-awesome-slider'; 
+import 'react-awesome-slider/dist/styles.css';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+
 import Slide1 from '../../assets/slide1.jpg';
 import Slide2 from '../../assets/slide2.jpg';
 import Slide3 from '../../assets/slide3.jpg';
 
+const Slider = withAutoplay(AwesomeSlider);
 
 const Home = () => {
-
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        }
-      };
-
 
     return (
         <div id="slideshow">
             <div id = "carousel">
-                <Carousel 
-                    responsive={responsive} 
-                    additionalTransfrom={0}
-                    arrows
-                    autoPlay={true}
-                    autoPlaySpeed={3000}
-                    centerMode={false}
-                    className="menu-grid"
-                    containerClass="container-with-dots"
-                    dotListClass=""
-                    draggable
-                    focusOnSelect={false}
-                    infinite
-                    itemClass="image-item"
-                    keyBoardControl
-                    minimumTouchDrag={80}
-                    renderButtonGroupOutside={false}
-                    renderDotsOutside={false}
-                    showDots={false}
-                    sliderClass=""
-                    slidesToSlide={2}
-                    swipeable>
+                <Slider
+                play
+                infinite
+                transitionDelay = {2000}
+                bullets={false}
+                >
                     <div className="slide">
                         <img className="slide-image" src={Slide1} alt='Slide 1' />
                     </div>
@@ -61,7 +29,7 @@ const Home = () => {
                     <div className="slide">
                         <img className="slide-image" src={Slide3} alt='Slide 3' />
                     </div>
-                </Carousel>
+                </Slider>
             </div>
         </div>
     )
