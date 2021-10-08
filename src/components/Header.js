1833ab8c3c20d34da9c33308f6615ae92651ef15
Andrew Sharkey <sharkand@hotmail.com>
2021-10-08 22:28:20 +0100

    Added FB and Twitter links.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Logo from '../assets/logo.jpg';
+import FbGrey from '../assets/FB_grey.png';
+import FbBlack from '../assets/FB_black.png';
+import TwitterGrey from '../assets/twitter_grey.png';
+import TwitterBlack from '../assets/twitter_black.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -10,6 +14,12 @@ const Header = () => {
                 <Link className="link" to="/">
                     <img src={Logo} alt='Logo'/>
                 </Link>
+                <a className="twitter" href="https://twitter.com/SwimInverclyde">
+                    <img src={TwitterGrey} alt="Twitter" id="twitter-img" /> 
+                </a>
+                <a className="FB" href="https://www.facebook.com/InverclydeMastersSwimmingClub">
+                    <img src={FbGrey} alt="Twitter" id="FB-img"/>
+                </a>
             </header>
         </div>
     )
