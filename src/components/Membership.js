7d4afe4422ff5b4d91b299f9ce2b0f9e7f881406
Andrew Sharkey <sharkand@hotmail.com>
2021-10-11 00:44:57 +0100

    Added media queries, news page, membership page.

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -3,9 +3,20 @@ import React from 'react';
 const Membership = () => {
 
     return (
-        <div className="container">
-            <h1>Membership</h1>
-        </div>
+        <>
+            <div className="main-section">
+                <header>
+                    <h1>MEMBERSHIP</h1>
+                </header>
+                <ul>
+                    <li>Annual Club Fees</li>
+                    <li>Membership Application Form</li>
+                    <li>Mandate Form</li>
+                    <li>Club Structure</li>
+                </ul>
+            </div>
+
+        </>
     )
 }
 
--- src/components/News.js
+++ src/components/News.js
@@ -3,9 +3,42 @@ import React from 'react';
 const News = () => {
 
     return (
-        <div className="container">
-            <h1>News & Events</h1>
-        </div>
+        <>
+            <div id="blog">
+                <header id="section-header">
+                    <h1>
+                        BLOG
+                    </h1>
+                </header>
+                
+                <section className="blog-item">
+                    <h2 className="title">
+                        GOUROCK POOL CLOSURE
+                    </h2>
+                    <h4 className="date">
+                        Monday 13th September 2021
+                    </h4>
+                </section>
+
+                <section className="blog-item">
+                    <h2 className="title">
+                        TOMMY IS A JOBBY
+                    </h2>
+                    <h4 className="date">
+                        Sunday 7th February 2021
+                    </h4>
+                </section>
+
+                <section className="blog-item">
+                    <h2 className="title">
+                        SWIM MEET CANCELLED
+                    </h2>
+                    <h4 className="date">
+                        Friday 19th June 2020
+                    </h4>
+                </section>
+            </div>
+        </>
     )
 }
 
