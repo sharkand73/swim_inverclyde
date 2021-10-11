f23a26c20d1204dcdd3c4ab63646f70cefaadf41
Andrew Sharkey <sharkand@hotmail.com>
2021-10-11 21:39:50 +0100

    Added About Us page.

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,85 @@ import React from 'react';
 const About = () => {
 
     return (
-        <div className="container">
-            <h1>About Us</h1>
-        </div>
+        <>
+            <div className="main-section">
+                <header>
+                    <h1>ABOUT US</h1>
+                </header>
+                <div id="two-column">
+                    <div>
+                        <h3>
+                            THE CLUB
+                        </h3>
+                        <p>
+                            The Club is a registered member of Scottish Swimming and offers coached practices for adults. All levels of swimmers aged 17 to 89+ are welcome to participate. We offer to develop our members swimming technique and strength through instruction and organised training. Available are structured practices 2 evenings and 2 weekend mornings per week where we work with the swimmers training schedules and individual goals, whether they compete as swimmers or just swim for personal fitness, so that each member can maximise their potential and attain their own objectives.
+                        </p>
+
+                        <h3>
+                            CLUB PHILOSOPHY
+                        </h3>
+                        <p>
+                            Inverclyde Masters Swim Club aims to maintain an environment that strikes a healthy balance between fitness, fun and competition. All levels of adult swimmers are welcome: from fitness swimmers to competitive pool and open water swimmers. The role of the Coach, is to design a programme that will allow all members to reach their individual goals. We believe that improving stroke technique and providing purposeful training sets are key elements to team and individual success. We also believe that having fun is just as important.
+                        </p>
+                        <p>
+                            The Club does not expect newcomers to be at any particular standard other than the ability to swim four lengths of a 25m pool in a reasonable standard. Inverclyde Masters being a Club means that all members have a purpose, which basically is to enjoy swimming as a fitness discipline.
+                        </p>
+                        <p>
+                        The Club is not over-burdened with rules. Club guidelines are only set because the training sessions have to be accommodated within public swimming pools. No one expects that swimmers attend every session however, membership of the Club implies a certain commitment and contribution to team spirit, (otherwise why bother joining any Club). As a Club Member swimmers would be expected to train as regularly as possible and join in with the team approach to training; and try out when you feel you are ready, some of the competitions that are held regularly throughout Scotland.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>
+                            Organisation of Practice
+                        </h3>
+                        <p>
+                            Workouts are divided into several skill levels so that each swimmer swims in a lane with people of similar ability. The sessions last for approximately 1 hour and offer a variety of training techniques and emphasis on stroke mechanics. Distances completed depend upon the ability group and can vary from 1000m to 3000m.
+                        </p>
+                        <p>
+                            New swimmers to the programme are encouraged to take plenty of rest time during workouts and initially not to push themselves to complete a full workout.
+                        </p>
+                        <p>
+                            The swim programme is designed to help each swimmer to enjoy and maintain continuity in a fitness programme that will be of benefit for the rest of their life.
+                        </p>
+                        <h3>
+                            Training Times
+                        </h3>
+                        <br/>
+                        <h4>
+                            May to September
+                        </h4>
+                        <p>
+                        <br/>
+                        Gourock Pool 33.33m (outdoor heated salt water pool)
+                        <br/>
+                        Albert Road, Gourock
+                        <br/>
+                        Tel. (01475) 631561
+                        </p>
+                        <p>
+                        Tuesday & Thursday 8.00pm – 9.00pm
+                        <br/>
+                        Saturday & Sunday 8:30am – 10.00am
+                        </p>
+                        <h4>
+                        September to May
+                        </h4>
+                        <p>
+                        <br/>
+                        The Waterfront Leisure Complex 25m
+                        Customhouse Way, Greenock
+                        Tel. (01475) 797979
+                        </p>
+                        <p>
+                        Tuesday & Thursday 8.30pm – 10.00pm
+                        <br/>
+                        Saturday & Sunday 8.00am – 9.00am
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
