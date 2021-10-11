9815a11d7bf975f857dd1244a86feb576b0297dd
Andrew Sharkey <sharkand@hotmail.com>
2021-10-11 20:53:06 +0100

    Added Other Activities page.

--- src/components/Other.js
+++ src/components/Other.js
@@ -3,9 +3,23 @@ import React from 'react';
 const Other = () => {
 
     return (
-        <div className="container">
-            <h1>Other Activities</h1>
-        </div>
+        <>
+            <div className="main-section">
+                <header>
+                    <h1>OTHER ACTIVTIES</h1>
+                </header>
+                <ul>
+                    <li>Swimming Competitions</li>
+                    <li>Open Water Swimming</li>
+                    <li>Cycling</li>
+                    <li>Triathlon</li>
+                </ul>
+                <footer>
+                    <p>Our community is growing and we have lots more to offer...</p>
+                </footer>
+            </div>
+
+        </>
     )
 }
 
