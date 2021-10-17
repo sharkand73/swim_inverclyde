830bfc36553194d2e95ae74d6ceeddfd9c4799f7
Andrew Sharkey <sharkand@hotmail.com>
2021-10-17 19:45:57 +0100

    Blog items now clickable.

--- src/components/News.js
+++ src/components/News.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
 
 const News = ({blog}) => {
 
+    const [currentBlog, setCurrentBlog] = useState(null);
+
     const blogItems = blog.map((item, index) => {
+        let formattedDate = moment(new Date(item.date)).format("dddd Do MMMM YYYY");
         return (
-            <section className="blog-item" key={index}>
+            <section className="blog-item" key={index} onClick = {() => setCurrentBlog(blog[index])}>
                     <h2 className="title">
                         {item.title}
                     </h2>
                     <h4 className="date">
-                        {item.date}
+                        {formattedDate}
                     </h4>
                 </section>
         )
     });
 
+    if (currentBlog) {
+        
+        return (
+            <>
+            <div id="blog">
+                <header id="section-header">
+                    <h1>
+                        BLOG
+                    </h1>
+                </header>
+                <section className="blog-item" onClick = {()=>setCurrentBlog(null)}>
+                    <h2 className="title">
+                        {currentBlog.title}
+                    </h2>
+                    <p className="body">
+                        {currentBlog.body}
+                    </p>
+                    <h4 className="date">
+                        {moment(new Date(currentBlog.date)).format("dddd Do MMMM YYYY")}
+                    </h4>
+                </section>
+            </div>
+        </>
+        )
+    }
     return (
         <>
             <div id="blog">
